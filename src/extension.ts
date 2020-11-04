a11ccbb46b154183fc9580d628832d690387c1f0
Yuancheng Zhang <old.far.hot@gmail.com>
2020-11-04 19:32:54 +0800

    update command and theme

--- src/extension.ts
+++ src/extension.ts
@@ -13,15 +13,15 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('project-da-vinci-vs-code-extension.helloWorld', () => {
+	const helloDaVinci = vscode.commands.registerCommand('project-da-vinci-vs-code-extension.helloDaVinci', () => {
 		// The code you place here will be executed every time your command is executed
 
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Project Da Vinci VS Code Extension!');
+		vscode.window.showInformationMessage('Hello Da Vinci!');
 	});
 
-	context.subscriptions.push(disposable);
+	context.subscriptions.push(helloDaVinci);
 }
 
 // this method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() { }
