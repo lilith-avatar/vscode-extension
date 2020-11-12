0fd51a086f96708c8c0a7be68d8f6f413625b813
Yuancheng Zhang <zhangyuancheng@lilith.com>
2020-11-12 18:31:00 +0800

    add test code tree view

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,24 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
+'use strict';
+
 import * as vscode from 'vscode';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
+import { CodeTreeProvider, Dependency } from './codeTree';
+
 export function activate(context: vscode.ExtensionContext) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "project-da-vinci-vs-code-extension" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
 	const helloDaVinci = vscode.commands.registerCommand('project-da-vinci-vs-code-extension.helloDaVinci', () => {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
 		vscode.window.showInformationMessage('Hello Da Vinci!');
 	});
 
 	context.subscriptions.push(helloDaVinci);
+
+	if (vscode.workspace.rootPath) {
+		const codeTreeProvider = new CodeTreeProvider(vscode.workspace.rootPath);
+		vscode.window.registerTreeDataProvider('codeTree', codeTreeProvider);
+		vscode.commands.registerCommand('codeTree.refresh', () => codeTreeProvider.refresh());
+	}
 }
 
 // this method is called when your extension is deactivated
